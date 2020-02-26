abb3bf84ed9a417948e38fc574d3f5df5fba9f30
oknashar <oknashar@gmail.com>
2020-02-26 17:03:07 +0200

    create a project

---
+++ chat_fire/chat_fire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chat_fire
+//
+//  Created by OKNASHAR on 2/26/20.
+//  Copyright © 2020 OKNASHAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
